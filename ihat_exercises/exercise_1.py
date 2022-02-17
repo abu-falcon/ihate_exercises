479cb8ea3da2b675fdc6856c159000612c823cba
abu-falcon <nopufa89@gmail.com>
2022-02-17 20:53:50 +0400

    initial commit

---
+++ ihat_exercises/exercise_1.py
@@ -0,0 +1,26 @@
+
+from typing import Any, TypeVar
+
+T = TypeVar("T")
+
+def repeat_word(word: str) -> str:
+    "space between them. If empty, the answer should be empty"
+    if not word:
+        return word 
+    return word + " " + word 
+
+def make_list(arg1: int, arg2: int, arg3: int) -> list[int]:
+    return [arg1, arg2, arg3]
+
+def make_tuple(arg1: int, arg2: int, arg3: int) -> tuple[int, int, int]:
+    return (arg1, arg2, arg3) 
+
+def make_dictionary(name: str, age: int, car: str) -> dict[str, str | int]:
+    return {"name": name, "age": age, "car": car}
+
+def find_number_in_list(number: int, mylist: list[int]) -> bool:
+    return number in mylist
+
+def find_value_of_key(key: int | str, d: dict[int | str, T]) -> T | None:
+    "If it can't find it, it should return None"
+    return d.get(key)
---
+++ tests/test_exercise_1.py
@@ -0,0 +1,66 @@
+import pytest
+from ihat_exercises.exercise_1 import (
+    find_number_in_list,
+    find_value_of_key,
+    make_dictionary,
+    make_list,
+    make_tuple,
+    repeat_word,
+)
+
+
+@pytest.fixture()
+def sample_dict():
+    sample_dict = {1: "one", "one": 1, "ls": [5, 7, 9]}
+    return sample_dict
+
+
+@pytest.mark.parametrize(
+    "word, expected", [("Water", "Water Water"), ("Yes", "Yes Yes"), ("", "")]
+)
+def test_repeat_word(word, expected) -> None:
+    assert repeat_word(word) == expected
+
+
+@pytest.mark.parametrize(
+    "arg1, arg2, arg3, expected",
+    [(1, 2, 3, [1, 2, 3]), (0, 0, 0, [0, 0, 0]), (1, 1, 2, [1, 1, 2])],
+)
+def test_make_list(arg1, arg2, arg3, expected) -> None:
+    ls = make_list(arg1, arg2, arg3)
+    assert ls == expected
+
+
+@pytest.mark.parametrize(
+    "arg1, arg2, arg3, expected",
+    [(1, 2, 3, (1, 2, 3)), (0, 0, 0, (0, 0, 0)), (1, 1, 2, (1, 1, 2))],
+)
+def test_make_tuple(arg1, arg2, arg3, expected) -> None:
+    t = make_tuple(arg1, arg2, arg3)
+    assert t == expected
+
+
+@pytest.mark.parametrize(
+    "arg",
+    [
+        {"name": "Ahmed", "age": 13, "car": "BMW"},
+        {"name": "Khalid", "age": 30, "car": "Nissan"},
+    ],
+)
+def test_make_dictionary(arg) -> None:
+    assert make_dictionary(**arg) == arg
+
+
+@pytest.mark.parametrize(
+    "number, ls, expected",
+    [(1, [3, 5, 1], True), (2, [2], True), (3, [2, 1], False), (5, [], False)],
+)
+def test_find_number_in_list(number, ls, expected) -> None:
+    assert find_number_in_list(number, ls) == expected
+
+
+@pytest.mark.parametrize(
+    "key, expected", [("one", 1), (1, "one"), ("hi", None), ("ls", [5, 7, 9])]
+)
+def test_find_value_of_key(key, expected, sample_dict) -> None:
+    assert find_value_of_key(key, sample_dict) == expected
